fac9fd608850599c0a067e4d94bc1a12212492ab
Jonathan Lamothe <jonathan@jlamothe.net>
2010-10-11 15:26:57 -0400

    Removed more unnecessary files

--- Arduinizer/Debug/src/cr_startup_lpc13.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc13.d src/cr_startup_lpc13.o: ../src/cr_startup_lpc13.c \
-  C:\Documents\ and\ Settings\amackie\My\ Documents\SVN\CMSISv1p30_LPC13xx\inc/system_LPC13xx.h
-
-C:\Documents\ and\ Settings\amackie\My\ Documents\SVN\CMSISv1p30_LPC13xx\inc/system_LPC13xx.h:
--- Arduinizer/Release/src/cr_startup_lpc13.d
+++
@@ -1,4 +0,0 @@
-src/cr_startup_lpc13.d src/cr_startup_lpc13.o: ../src/cr_startup_lpc13.c \
- /home/jlamothe/src/arduinizer/CMSISv1p30_LPC13xx/inc/system_LPC13xx.h
-
-/home/jlamothe/src/arduinizer/CMSISv1p30_LPC13xx/inc/system_LPC13xx.h:
